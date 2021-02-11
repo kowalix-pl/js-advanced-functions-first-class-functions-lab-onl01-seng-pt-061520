f6e5e7c9eff6356ce80fda946e05292fee4e1687
Lukasz Kowalik <kowalix007@gmail.com>
2021-02-11 22:36:35 +0000

    commit tree b35113a0a470b400e7ffb1c9bed5c8cdbd77c2a6

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(array){
+  const newArray = [];
+  newArray.push(array[0]);
+  newArray.push(array[1]);
+  return newArray
+}
+function returnLastTwoDrivers(array){
+ return array.slice(array.length-2);
+}
+
+const selectingDrivers = []
+selectingDrivers[0]= returnFirstTwoDrivers();
+selectingDrivers[selectingDrivers.length-1]=returnLastTwoDrivers();
+
